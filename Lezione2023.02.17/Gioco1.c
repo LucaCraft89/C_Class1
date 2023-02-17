2923eeb747b54d115fa1dc267aa9d56f35405c52
Luca Dall'Acqua <85185929+LucaCraft89@users.noreply.github.com>
2023-02-17 17:03:06 +0000

    Terminato Gioco1.c

--- Lezione2023.02.17/Gioco1.c
+++ Lezione2023.02.17/Gioco1.c
@@ -5,34 +5,42 @@
 int main()
 {
     int n, t, m, lp;
-    srand(time(NULL));
-    n = rand()%100+1;
-    printf("Cercare di indovinare il numero compreso fra 1 e 100\n");
-    printf("%d", n); printf("\n");
-    for (lp = 2; lp > -1; lp--)
-    {
-        if(lp==2){
-            scanf("%d", &m);
-        }
-        else{
-            printf("Hai sbagliato, riprova, hai ancora "); printf("%d", lp+1); printf(" tentativi \n");
-            if (m>n) {
-                printf("il numero è più basso \n");
+    char c='y';
+    while (c=='y' || c=='Y'){
+        srand(time(NULL));
+        n = rand()%100+1;
+        printf("Cercare di indovinare il numero compreso fra 1 e 100\n");
+        printf("%d", n); printf("\n");
+        for (lp = 2; lp > -1; lp--)
+        {
+            if(lp==2){
+                scanf("%d", &m);
             }
             else{
-                printf("il numero è più alto \n");
+                printf("Hai sbagliato, riprova, hai ancora "); printf("%d", lp+1); printf(" tentativi \n");
+                if (m>n) {
+                    printf("il numero è più basso \n");
+                }
+                else{
+                    printf("il numero è più alto \n");
+                }
+            scanf("%d", &m);
             }
-        scanf("%d", &m);
-        }
-        if (m==n) {
-            printf("hai vinto \n");
-            break;
-        }
-        else{
-            if (lp==0){
-                printf("il numero era: "); printf("%d", n); printf("\n");
+            if (m==n) {
+                printf("hai vinto \n");
+                printf("Vuoi giocare ancora? (y = si, qualsiasi altra cosa = no) \n");
+                scanf("%s", &c);
+                break;
             }
-        }
-    }   
+            else{
+                if (lp==0){
+                    printf("il numero era: "); printf("%d", n); printf("\n");
+                    printf("Vuoi giocare ancora? (y = si, qualsiasi altra cosa = no) \n");
+                    scanf("%s", &c);
+                    break;
+                }
+            }
+        }   
+    }
     return 0;
 }
