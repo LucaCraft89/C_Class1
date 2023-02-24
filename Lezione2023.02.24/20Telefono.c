c6843b210c60e0e7d0d19423adf66b522bf15cfe
admin <ertoh2o@gmail.com>
2023-02-24 13:32:36 +0000

    sdf

--- Lezione2023.02.24/20Telefono.c
+++ Lezione2023.02.24/20Telefono.c
@@ -8,10 +8,39 @@ int main()
     printf("inserire il proprio nome \n"); 
     scanf("%s", &n);
     printf("inserire gli scatti della bollte precedente \n"); scanf("%d", &bp);
+    while (bp<0){
+        printf("non può essere minore di 0 \n");
+        printf("inserire gli scatti della bollte precedente \n"); scanf("%d", &bp);
+    }
     printf("inserire gli scatti della bollte attual \n"); scanf("%d", &ba);
+    while (ba<0){
+        printf("non può essere minore di 0 \n");
+        printf("inserire gli scatti della bollte attual \n"); scanf("%d", &ba);
+    }
+    while (ba<bp){
+        printf("la bolletta attuale deve essere più grande");
+        printf("inserire gli scatti della bollte precedente \n"); scanf("%d", &bp);
+        while (bp<0){
+            printf("non può essere minore di 0 \n");
+            printf("inserire gli scatti della bollte precedente \n"); scanf("%d", &bp);
+        }
+        printf("inserire gli scatti della bollte attual \n"); scanf("%d", &ba);
+        while (ba<0){
+            printf("non può essere minore di 0 \n");
+            printf("inserire gli scatti della bollte attual \n"); scanf("%d", &ba);
+    }
+    }
     printf("inserire canone fisso \n"); scanf("%d", &cf);
+    while (cf<0){
+        printf("non può essere minore di 0 \n");
+        printf("inserire canone fisso \n"); scanf("%d", &cf);
+    }
     printf("inserire costo di ogni sactto \n"); scanf("%d", &cs);
+    while (cs<0){
+        printf("non può essere minore di 0 \n");
+        printf("inserire costo di ogni sactto \n"); scanf("%d", &cs);
+    }
     paga=((ba-bp)*cs)+cf;
-     printf("%s", n); printf(" hai da pagare: %d \n", paga);
+    printf("%s", n); printf(" hai da pagare: %d \n", paga);
     return 0;
 }
