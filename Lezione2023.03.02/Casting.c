423f18b8ed5f3e2ad13677da6ac0dd7f5b17795e
LucaCraft89 <luca07h2o@gmail.com>
2023-03-02 12:45:20 +0100

    kjhkjh

--- Lezione2023.03.02/Casting.c
+++ Lezione2023.03.02/Casting.c
@@ -3,5 +3,11 @@
 
 int main()
 {
+    int a, b;
+    float c;
+    printf("inerisci a: \n"); scanf("%d", &a);
+    printf("inerisci b: \n"); scanf("%d", &b);
+    c=(float)a/b;
+    printf("c vale: %.2f \n", c);
     return 0;
 }
