47a381862e377e04ab7f0d52999620bd688c0db5
Luca Dall'Acqua <85185929+LucaCraft89@users.noreply.github.com>
2023-03-17 14:52:09 +0100

    chweknon

--- Lezione2023.03.17/es40.c
+++ Lezione2023.03.17/es40.c
@@ -3,6 +3,19 @@
 
 int main()
 {
-    
+    int a, i, x, n, s,x1,x2;
+    printf("a: \n"); scanf("%d", &a);
+    printf("x: \n"); scanf("%d", &x); x1=x;
+    i=0;
+    x=a+1;
+    x2=x+1;
+    do
+    {
+        x=x+x2;
+        x2=x;
+        i=i+1;
+        
+    } while (x<x1);
+    printf("Interazioni: %d \n", i+1);
     return 0;
 }
