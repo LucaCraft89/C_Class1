47ae48cc19e0511b7f79bd8cfd1ebf38c2df085a
Luca Dall'Acqua <85185929+LucaCraft89@users.noreply.github.com>
2023-03-23 12:25:22 +0100

    vsdoij

---
+++ Lezione2023.03.23/whilexapml.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, a; i=0;
+    printf("Iserisci il numero: \n"); scanf("%d", &a);
+    while (a<=0)
+    {
+        printf("il numero non può essere minore o uguale a 0 \n");
+        printf("Iserisci il numero: \n"); scanf("%d", &a);
+    }
+    printf("I numeri che vengono prima di %d sono: \n", a);
+    while (i<a)
+    {
+        printf("%d \t", i);
+        i=i+1;
+    }
+    
+    return 0;
+}  
